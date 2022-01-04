10976f2e6e1ea97e7cfb84ef9b877116df4c5ff2
seo1163 <seo1163@naver.com>
2022-01-04 12:10:49 +0900

    JavaTest1

---
+++ src/com/iu/s1/test/JavaStudyTest.java
@@ -0,0 +1,13 @@
+package com.iu.s1.test;
+
+public class JavaStudyTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+		
+		System.out.println("Finish");
+		
+	}
+
+}
